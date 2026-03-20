c48403352fdf6897c1c79b7c87cf39569a7493ac
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-20 14:00:38 +0530

    base file setup

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
