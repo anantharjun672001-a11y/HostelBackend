c3c94a253af72d04f5a6db4e3a3131fa027a9acc
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-27 20:42:17 +0530

    payment routes altered

--- routes/payment.js
+++ routes/payment.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {
   createOrder,
-  razorpayWebhook,
+  
 } from "../controllers/paymentController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 
-router.post("/webhook", razorpayWebhook);
+/* router.post("/webhook", razorpayWebhook); */
 router.post("/create-order", verifyToken, createOrder);
 
 export default router;
